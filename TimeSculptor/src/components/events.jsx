a5c0920e410446f5f6c4f5f426411bdf6318bc8f
Izaac Molina <izaac.6.30@gmail.com>
2023-10-22 17:25:40 -0700

    started work on first app iteration, implemented header, create event button, and placeholder for the events

---
+++ TimeSculptor/src/components/events.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './events.css'
+
+export default function Events() { 
+    return (
+        <div className="eventsDisplay">
+            Events
+        </div>
+    )
+}
