d8c6bccd3cc1ecca3360dd3744f9c05b800ff220
Joey-Banaszak <95311566+Joey-Banaszak@users.noreply.github.com>
2023-10-26 20:41:03 -0700

    Update test_createEvent.test.js

--- TimeSculptor/public/assets/__tests__/test_createEvent.test.js
+++ TimeSculptor/public/assets/__tests__/test_createEvent.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import AddEvent from './AddEvent';
+import AddEvent from '../AddEvent';
 
 describe('AddEvent component', () => {
-  it('renders the AddEvent component with a button', () => {
+  it('renders the add event button', () => {
     render(<AddEvent addEventFunction={() => {}} />);
 
     // Check if the "Add Event" button is in the window
     const addButton = screen.getByText('Add Event');
     expect(addButton).toBeInTheDocument();
-  });
+  });Dialog
 
   it('opens the event dialog when the button is clicked', () => {
     render(<AddEvent addEventFunction={() => {}} />);
@@ -21,8 +21,8 @@ describe('AddEvent component', () => {
     fireEvent.click(addButton);
 
     // Check if the event dialog is opened
-    const eventDialog = screen.getByTestId('event-dialog');
-    expect(eventDialog).toBeInTheDocument();
+    const eventMessage = screen.getByTestId('event-dialog');
+    expect(eventMessage).toBeInTheDocument();
   });
 
   it('closes the event dialog when the close function is called', () => {
@@ -35,14 +35,14 @@ describe('AddEvent component', () => {
     fireEvent.click(addButton);
 
     // Check if the event dialog is opened
-    const eventDialog = screen.getByTestId('event-dialog');
-    expect(eventDialog).toBeInTheDocument();
+    const eventMessage = screen.getByTestId('event-dialog');
+    expect(eventMessage).toBeInTheDocument();
 
     // Close the dialog by calling the close function
     const closeButton = screen.getByTestId('close-button');
     fireEvent.click(closeButton);
 
     // Check if the event dialog is closed
-    expect(eventDialog).not.toBeInTheDocument();
+    expect(eventMessage).not.toBeInTheDocument();
   });
 });
