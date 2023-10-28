68b2a84908e4c27632862407d1ebd8515daae290
Joey-Banaszak <95311566+Joey-Banaszak@users.noreply.github.com>
2023-10-28 14:44:00 -0700

    Update events.test.js

--- TimeSculptor/src/components/__tests__/events.test.js
+++ TimeSculptor/src/components/__tests__/events.test.js
@@ -1,24 +1,28 @@
 const EventTable = require("../events")
 
+// create a test event to input into the table 
 const testEvent1 = {
   title: "test event 1",
   time: "12:00 PM",
   date: "2057-10-24",
 };
+// create another test event to input into the table
 const testEvent2 = {
   title: "test event 2",
   time: "1:59 PM",
   date: "2001-9-10",
 };
 
+// test to see that an event can be added to the table 
 test("creating an event in the future should work", () => {
   const{ daTest1 } = EventTable( testEvent1 );
 
   expect(daTest1).toEqual(testEvent1);
 })
 
+// test to see that a second event also works in the function
 test("creating an event", () => {
-  const{ daTest2 } = EventTable( testEvent2, false, false );
+  const{ daTest2 } = EventTable( testEvent2 );
 
   expect(daTest2).toEqual(testEvent2);
 })
