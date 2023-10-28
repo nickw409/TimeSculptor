0dea66ff915cface04c9db4f05fd00a039df1575
Nick Wiley <nmw264@nau.edu>
2023-10-28 09:21:17 -0700

    Next day fixes

--- TimeSculptor/auth_server.cjs
+++ TimeSculptor/auth_server.cjs
@@ -8,7 +8,7 @@ const port = 6969;
 var jsonParser = bodyParser.json();
 
 app.use(cors({
-  origin:['http:enginick.com:9696', 'http://142.11.234.231:9696', 'http://localhost:9696'],
+  origin:['*'],
   credentials:true
 }));
 
@@ -18,10 +18,6 @@ app.use('/login', jsonParser, cors(), (req, res) => {
   console.log(req.body);
   if(req.body?.username !== undefined && req.body.username === 'admin') {
     if(req.body?.password !== undefined && req.body.password === 'password') {
-      res.setHeader('Access-Control-Allow-Origin', "*");
-      res.setHeader('Access-Control-Allow-Headers', true);
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.send({
         token: 'admin'
       });
