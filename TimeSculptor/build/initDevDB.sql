1ec03c6fb173276a37bde5db5a01b45188617abf
nickw409 <nwiley333@gmail.com>
2023-11-19 21:10:58 -0700

    Working on get-event access point

--- TimeSculptor/build/initDevDB.sql
+++ TimeSculptor/build/initDevDB.sql
@@ -4,6 +4,7 @@
 -- Make sure to use password for root user in db_init.sh
 -- DDL Statements
 -- --------------
+DROP DATABASE IF EXISTS dev_db;
 CREATE DATABASE IF NOT EXISTS dev_db;
 
 CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY 'TimeSculptor';
@@ -12,10 +13,30 @@ GRANT ALL PRIVILEGES ON dev_db.* TO 'dev'@'localhost';
 USE dev_db;
 
 CREATE TABLE IF NOT EXISTS Credential (
-   username VARCHAR(128) NOT NULL,
+   username VARCHAR(128) PRIMARY KEY NOT NULL,
    password VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Schedule (
+   schedule_name VARCHAR(128) PRIMARY KEY NOT NULL,
+   username VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Event (
+   schedule_name VARCHAR(128) NOT NULL,
+   id VARCHAR(32) PRIMARY KEY NOT NULL,
+   event VARCHAR(256)
 );
 
 INSERT IGNORE INTO Credential
 SET   username = 'admin',
       password = 'password';
+
+INSERT IGNORE INTO Schedule
+SET   schedule_name  = 'testing',
+      username       = 'admin';
+
+INSERT IGNORE INTO Event
+SET   schedule_name  = 'testing',
+      id             = '1',
+      event          = 'placeholder';
